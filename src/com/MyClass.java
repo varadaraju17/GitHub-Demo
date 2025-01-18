4d8390dd766835f4f3a45b6ff4a00d1e86e1e680
varadaraju17 <varada883@gmail.com>
2025-01-18 20:05:05 +0530

    First Commit

---
+++ src/com/MyClass.java
@@ -0,0 +1,11 @@
+package com;
+
+public class MyClass {
+	 static void myCar(){
+		System.out.println(" my  car");
+	}
+public static void main(String[] args) {
+	System.out.println("Hello");
+     myCar();
+}
+}
